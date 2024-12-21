5884b34678d5b6c9eacc66a0876113ff62d926ee
Johannes (Jotrorox) Müller <mail@jotrorox.com>
2024-12-21 22:58:47 +0100

    Change to index.html single file app

--- docker-compose-build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Export the current user and group IDs
-export USER_ID=$(id -u)
-export GROUP_ID=$(id -g)
-
-# Run docker-compose with the current user/group IDs
-docker compose up --build "$@" 
